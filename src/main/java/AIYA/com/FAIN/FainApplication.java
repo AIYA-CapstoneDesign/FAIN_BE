5fd3f5248893fd116235da4c7febefdd75433e8b
wccjcc <goqkfkrltl12@naver.com>
2025-05-02 00:11:41 +0900

    Initialize develop branch

---
+++ src/main/java/AIYA/com/FAIN/FainApplication.java
@@ -0,0 +1,13 @@
+package AIYA.com.FAIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AIYA/com/FAIN/FainApplicationTests.java
@@ -0,0 +1,13 @@
+package AIYA.com.FAIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
