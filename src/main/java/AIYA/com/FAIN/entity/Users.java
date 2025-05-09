fd6709c307a4bbc14fbf6ebe0a814c5454451fac
dodohan1224 <kdymmm1224@naver.com>
2025-05-09 23:01:48 +0900

    [feat] 회원 DB 구현

---
+++ src/main/java/AIYA/com/FAIN/entity/Users.java
@@ -0,0 +1,64 @@
+package AIYA.com.FAIN.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter // 각 모든 필드에 대해 Getter 생성
+@Setter
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자
+public class Users {
+
+  @Id
+  @Column(nullable=false, unique=true)
+  private long user_id; //보호자 아이디
+
+  @Column(nullable=false)
+  private String password; // 보호자 비밀번호
+
+  @Column(nullable=false)
+  private String f_name;
+
+  @Column(nullable=false)
+  private String f_tel;
+
+  @Column(nullable=false)
+  private String name;
+
+  @Column(nullable=false)
+  private LocalDate birth;
+
+  @Column(nullable=false)
+  private String zone_code;
+
+  @Column(nullable=false)
+  private String road_address;
+
+  private String detail_address;
+
+  @Column(nullable=false)
+  private String bloodtype;
+
+  private String disease;
+
+  private String hospital_name;
+
+  private String hospital_tel;
+
+  private String height;
+
+  private String weight;
+
+  private String allergic;
+
+  private String medicine;
+
+
+}
