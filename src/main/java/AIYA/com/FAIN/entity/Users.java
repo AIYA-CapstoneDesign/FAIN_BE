aee5b2e7b54ee3d73848b8499c46d54f322aa21f
dodohan1224 <kdymmm1224@naver.com>
2025-05-17 12:56:01 +0900

    [refactor] 회원 entity id추가 및 기본키 설정, autoincrement 설정 추가 , user_id 타입 변경

--- src/main/java/AIYA/com/FAIN/entity/Users.java
+++ src/main/java/AIYA/com/FAIN/entity/Users.java
@@ -2,6 +2,8 @@ package AIYA.com.FAIN.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
@@ -17,8 +19,11 @@ import lombok.Setter;
 public class Users {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id; // 회원번호
+
   @Column(nullable=false, unique=true)
-  private long user_id; //보호자 아이디
+  private String user_id; //보호자 아이디
 
   @Column(nullable=false)
   private String password; // 보호자 비밀번호
