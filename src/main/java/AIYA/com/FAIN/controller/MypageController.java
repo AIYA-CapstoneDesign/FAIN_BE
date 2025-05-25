07fff62483df8164be998f04dfc91f981b45d85e
dodohan1224 <kdymmm1224@naver.com>
2025-05-25 15:14:01 +0900

    [feat] 정보수정 기능 구현

--- src/main/java/AIYA/com/FAIN/controller/MypageController.java
+++ src/main/java/AIYA/com/FAIN/controller/MypageController.java
@@ -1,10 +1,13 @@
 package AIYA.com.FAIN.controller;
 
 import AIYA.com.FAIN.dto.MypageResponseDto;
+import AIYA.com.FAIN.dto.MypageUpdateRequestDto;
 import AIYA.com.FAIN.jwt.JwtUtil;
 import AIYA.com.FAIN.service.MypageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,19 @@ public class MypageController {
     MypageResponseDto responseDto = mypageService.getMyPage(userId);
 
     return ResponseEntity.ok(responseDto);
+  }
+
+  @PatchMapping("/api/v1/updateProfiles")
+  public ResponseEntity<String> updateProfiles(@RequestHeader("Authorization") String header,
+      @RequestBody MypageUpdateRequestDto dto){
+    String token = header;
+    if (header.startsWith("Bearer ")) {
+      token = header.substring(7).trim(); // "Bearer " 자르고 trim()으로 공백없애기
+    }
+    String userId = jwtUtil.getUserIdFromToken(token);
+
+    mypageService.UpdateMypage(userId,dto);
+    return ResponseEntity.ok("success");
   }
 
 }
--- src/main/java/AIYA/com/FAIN/service/MypageService.java
+++ src/main/java/AIYA/com/FAIN/service/MypageService.java
@@ -2,9 +2,11 @@ package AIYA.com.FAIN.service;
 
 import AIYA.com.FAIN.dto.GuardianDto;
 import AIYA.com.FAIN.dto.MypageResponseDto;
+import AIYA.com.FAIN.dto.MypageUpdateRequestDto;
 import AIYA.com.FAIN.dto.PatientDto;
 import AIYA.com.FAIN.entity.Users;
 import AIYA.com.FAIN.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,6 +20,7 @@ public class MypageService {
     this.userRepository = userRepository;
   }
 
+  @Transactional
   public MypageResponseDto getMyPage(String userId){
     final Users user = userRepository
         .findByUserId(userId).orElseThrow(()->{System.out.println("userId 못 찾음: '" + userId + "'");
@@ -43,6 +46,46 @@ public class MypageService {
     );
 
     return new MypageResponseDto(guardianDto, patientDto);
+  }
+
+  @Transactional
+  public void UpdateMypage(String userId, MypageUpdateRequestDto dto){
+    Users users =  userRepository.findByUserId(userId)
+        .orElseThrow(()-> new RuntimeException("사용자를 찾을 수 없음"));
+
+    if (dto.getFName() != null) {
+      users.setFName(dto.getFName());
+    }
+    if (dto.getFTel() != null) {
+      users.setFTel(dto.getFTel());
+    }
+    if(dto.getName() != null) {
+      users.setName(dto.getName());
+    }
+    if(dto.getBirth() != null) {
+      users.setBirth(dto.getBirth());
+    }
+    if(dto.getAddress() != null) {
+      users.setAddress(dto.getAddress());
+    }
+    if(dto.getHeight() != null) {
+      users.setHeight(dto.getHeight());
+    }
+    if(dto.getWeight() != null) {
+      users.setWeight(dto.getWeight());
+    }
+    if(dto.getBloodType() != null) {
+      users.setBloodtype(dto.getBloodType());
+    }
+    if(dto.getAllergic() != null) {
+      users.setAllergic(dto.getAllergic());
+    }
+    if(dto.getMedicine() != null) {
+      users.setMedicine(dto.getMedicine());
+    }
+    if(dto.getHospitalName() != null) {
+      users.setHospitalName(dto.getHospitalName());
+    }
   }
 
 
