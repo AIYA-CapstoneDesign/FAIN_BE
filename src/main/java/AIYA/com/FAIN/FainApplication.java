1d61903b070a048f273df5e17e3b8e6a1324888d
wccjcc <goqkfkrltl12@naver.com>
2025-05-30 16:56:33 +0900

    [chore]swagger 주소 https로 고정

--- src/main/java/AIYA/com/FAIN/FainApplication.java
+++ src/main/java/AIYA/com/FAIN/FainApplication.java
@@ -1,8 +1,15 @@
 package AIYA.com.FAIN;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @io.swagger.v3.oas.annotations.info.Info(title = "FAIN API", version ="v1"),
+		servers = {
+				@Server(url="https://fain-aiya.shop",description="배포서버")
+		}
+)
 @SpringBootApplication
 public class FainApplication {
 
--- src/main/java/AIYA/com/FAIN/config/SwaggerConfig.java
+++ src/main/java/AIYA/com/FAIN/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package AIYA.com.FAIN.config;
 
+
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
