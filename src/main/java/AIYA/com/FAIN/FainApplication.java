674430c0791bbc0e810548cc0f1b97412e77457f
wccjcc <goqkfkrltl12@naver.com>
2025-06-01 00:36:57 +0900

    [chore]swagger config수정6

--- src/main/java/AIYA/com/FAIN/FainApplication.java
+++ src/main/java/AIYA/com/FAIN/FainApplication.java
@@ -1,7 +1,6 @@
 package AIYA.com.FAIN;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
