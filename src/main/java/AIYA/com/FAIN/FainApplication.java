eb0e7d595e314cd2c96cb76c7fd239a08b8529c8
wccjcc <goqkfkrltl12@naver.com>
2025-05-31 23:34:10 +0900

    [chore]swagger https설정

--- src/main/java/AIYA/com/FAIN/FainApplication.java
+++ src/main/java/AIYA/com/FAIN/FainApplication.java
@@ -4,12 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@OpenAPIDefinition(
-		info = @io.swagger.v3.oas.annotations.info.Info(title = "FAIN API", version ="v1"),
-		servers = {
-				@Server(url="https://fain-aiya.shop",description="배포서버")
-		}
-)
+@OpenAPIDefinition(servers = {
+		@Server(url="https://fain-aiya.shop",description = "Default Server Url")
+})
 @SpringBootApplication
 public class FainApplication {
 
