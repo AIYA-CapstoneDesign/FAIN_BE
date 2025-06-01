c26023ae9f6eb487c37b4e15cb15fbca92295796
wccjcc <goqkfkrltl12@naver.com>
2025-06-01 10:31:18 +0900

    [refactor]Swagger httpst시도

--- src/main/java/AIYA/com/FAIN/config/SwaggerConfig.java
+++ src/main/java/AIYA/com/FAIN/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -29,6 +31,9 @@ public class SwaggerConfig {
         .info(new Info()
             .title("FAIN Swagger")
             .description("FAIN REST API")
-            .version("1.0.0"));
+            .version("1.0.0"))
+        .servers(List.of(
+            new Server().url("https://fain-aiya.shop").description("FAIN배포서버")
+        ));
   }
 }
