58950e47651b259008f256b8701081edfa9843a5
wccjcc <goqkfkrltl12@naver.com>
2025-06-02 17:37:51 +0900

    [feat] 히스토리 상세조회

--- src/main/java/AIYA/com/FAIN/controller/HistoryController.java
+++ src/main/java/AIYA/com/FAIN/controller/HistoryController.java
@@ -15,11 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class HistoryController {
 
   private final HistoryService historyService;
