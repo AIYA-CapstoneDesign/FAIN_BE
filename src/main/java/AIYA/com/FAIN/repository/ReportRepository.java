3200bb815e5e4731ea56885ea6ae6191d3bb43ca
wccjcc <goqkfkrltl12@naver.com>
2025-06-02 14:36:18 +0900

    [refactor] History간략조회 수정

--- src/main/java/AIYA/com/FAIN/repository/ReportRepository.java
+++ src/main/java/AIYA/com/FAIN/repository/ReportRepository.java
@@ -1,9 +1,12 @@
 package AIYA.com.FAIN.repository;
 
 import AIYA.com.FAIN.entity.Reports;
+import AIYA.com.FAIN.entity.Users;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Reports,Long> {
   List<Reports> findAllByUserId(Integer id);
+
+  List<Reports> findAllByUser(Users user);
 }
