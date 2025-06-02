f0c59ec6ea31386c1c219f0050719667b9161898
wccjcc <goqkfkrltl12@naver.com>
2025-06-02 17:45:45 +0900

    [chore] OPTIONS요청 처리

--- src/main/java/AIYA/com/FAIN/config/SecurityConfig.java
+++ src/main/java/AIYA/com/FAIN/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import AIYA.com.FAIN.jwt.JwtUtil;
 import AIYA.com.FAIN.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -65,6 +66,7 @@ public class SecurityConfig {
 
     // 허용해주는 API
     http.authorizeHttpRequests((auth) -> auth
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
         .requestMatchers(
             "/api/v1/login",
             "/",
