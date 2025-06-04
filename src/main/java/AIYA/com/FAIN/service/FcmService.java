b5fb88a3991cccbfd04d05596f4973a1772a8628
dodohan1224 <kdymmm1224@naver.com>
2025-06-04 23:28:37 +0900

    [refactor] fcm 토큰

--- src/main/java/AIYA/com/FAIN/service/FcmService.java
+++ src/main/java/AIYA/com/FAIN/service/FcmService.java
@@ -3,6 +3,7 @@ package AIYA.com.FAIN.service;
 import AIYA.com.FAIN.entity.FcmToken;
 import AIYA.com.FAIN.repository.FcmTokenRepository;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import java.util.List;
@@ -34,11 +35,28 @@ public class FcmService {
               .build())
           .putData("reportId",reportId.toString()).build();
 
-      try{
+//      try{
+//        FirebaseMessaging.getInstance().send(message);
+//      }catch (Exception e){
+//        e.printStackTrace();
+//      }
+      try {
         FirebaseMessaging.getInstance().send(message);
-      }catch (Exception e){
+      } catch (FirebaseMessagingException e) {
+        String errMsg = e.getMessage();
+        System.err.println("FCM 전송 실패 - 토큰: " + token.getToken());
+        e.printStackTrace();
+
+        // 유효하지 않은 FCM 토큰이면 삭제해도 됨 (선택)
+        if ("INVALID_ARGUMENT".equals(e.getMessagingErrorCode().name())) {
+          System.err.println("❌ 유효하지 않은 토큰 → DB에서 삭제");
+          fcmTokenRepository.delete(token); // optional
+        }
+      } catch (Exception e) {
+        System.err.println("FCM 전송 중 알 수 없는 에러 발생");
         e.printStackTrace();
       }
+
     }
 
   }
