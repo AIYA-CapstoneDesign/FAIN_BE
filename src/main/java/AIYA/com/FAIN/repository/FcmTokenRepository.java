f0863bbd3631f7886e5d01e8949cf87e8d57cda7
dodohan1224 <kdymmm1224@naver.com>
2025-06-05 16:04:29 +0900

    [refactor] FcmTokenRepository에다가 transactional 추가

--- src/main/java/AIYA/com/FAIN/repository/FcmTokenRepository.java
+++ src/main/java/AIYA/com/FAIN/repository/FcmTokenRepository.java
@@ -5,11 +5,13 @@ import AIYA.com.FAIN.entity.Users;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 // DB와 연결해 데이터를 읽고 쓰는 역할
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
   Optional<FcmToken> findByToken(String token); // 토큰 중복 방지를 위해 조회메서드
   List<FcmToken> findAllByUser(Users user); // 유저의 모든 토큰 찾기
+  @Transactional
   void deleteAllByUser(Users user);
 
 }
